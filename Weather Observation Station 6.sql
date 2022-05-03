ad7a8f5846ba42dc42436711da0518e381e9c761
Pedro Pelacani Berger <98188778+pedropberger@users.noreply.github.com>
2022-05-03 14:59:26 -0300

    Create Weather Observation Station 6.sql
    Easy question
    The plus here is the use of 'a%' for selection of vowels

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,5 @@
+-- The plus here is the use of 'a%' for selection of vowels
+
+SELECT CITY
+FROM STATION
+WHERE CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%'
