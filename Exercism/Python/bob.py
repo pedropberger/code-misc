24d5078f63262e3b05a7e19261ed4f8db5751f64
Pedro Pelacani Berger <98188778+pedropberger@users.noreply.github.com>
2022-09-21 19:59:36 -0300

    Create bob.py

---
+++ Exercism/Python/bob.py
@@ -0,0 +1,15 @@
+def response(hey_bob):
+    
+    if hey_bob.endswith('?') and hey_bob.isupper():
+        return "Calm down, I know what I'm doing!"
+    elif hey_bob.endswith('?'):
+        return 'Sure.'
+    elif hey_bob.isupper():
+        return 'Whoa, chill out!'
+    elif hey_bob == 'Bob':
+        return 'Fine. Be that way!'
+    elif hey_bob == '':
+        return 'Fine. Be that way!'
+    else:
+        return 'Whatever.'
+        
