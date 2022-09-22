6de74a09df6142727b52c3057e37876bab4d2544
Pedro Pelacani Berger <98188778+pedropberger@users.noreply.github.com>
2022-09-22 20:02:32 -0300

    Update bob.py

--- Exercism/Python/bob.py
+++ Exercism/Python/bob.py
@@ -2,7 +2,7 @@ def response(hey_bob):
     
     if hey_bob.endswith('?') and hey_bob.isupper():
         return "Calm down, I know what I'm doing!"
-    elif hey_bob.endswith('?'):
+    elif hey_bob.strip().endswith('?'):
         return 'Sure.'
     elif hey_bob.isupper():
         return 'Whoa, chill out!'
@@ -10,6 +10,7 @@ def response(hey_bob):
         return 'Fine. Be that way!'
     elif hey_bob == '':
         return 'Fine. Be that way!'
+    elif hey_bob.isspace():
+        return 'Fine. Be that way!'
     else:
         return 'Whatever.'
-        
